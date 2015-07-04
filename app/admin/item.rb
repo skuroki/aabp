b0e5f5683c0bc4ea46a413d01d702d02cba7faac
Shinsuke KUROKI <s-kuroki@aiming-inc.com>
2015-07-04 20:27:17 +0900

    Add item admin page settings

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Item do
+  permit_params :name, :price
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  # 省略するとid, created_at, updated_atも含めた全項目が出ます
+  index do
+    column :name
+    column :price
+    actions
+  end
 
+  # 省略するとid, created_at, updated_atも含めた全項目が出ます
+  show do
+    attributes_table do
+      row :name
+      row :price
+    end
+  end
 
+  # 省略してもだいたい同じものが出てきます
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :price
+    end
+    f.actions
+  end
 end
